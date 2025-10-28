98a0a4d5a0b8df16790e28ff46ef01416ea515d8
Elías Vasquez <78238476+Evasq710@users.noreply.github.com>
2025-10-28 14:24:00 -0600

    02-template-string

---
+++ 01-reforzamiento/src/bases/02-template-string.ts
@@ -0,0 +1,8 @@
+const firstName = 'Elías';
+const lastName = "O'Neal \"es el apellido de alguien\"";
+
+const fullName = `El nombre completo es:
+    ${firstName} ${lastName} ${1 + 1}
+`;
+
+console.log(fullName);

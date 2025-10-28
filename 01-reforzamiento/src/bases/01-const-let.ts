eab50842777a6423668024fc5e491f6b2cf51e39
Elías Vasquez <78238476+Evasq710@users.noreply.github.com>
2025-10-28 14:17:21 -0600

    01-const-let

---
+++ 01-reforzamiento/src/bases/01-const-let.ts
@@ -0,0 +1,10 @@
+// No usar var. Usar let (variable) o const (constantes)
+
+const firstName: string = 'Elías';
+const lastName = 'Vasquez';
+
+let diceNumber = 5;
+diceNumber = 3;
+
+console.log('My name is', firstName, lastName);
+console.log({diceNumber});

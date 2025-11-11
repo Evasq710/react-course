668f3c3c94e005898e80c354f5f514e0faeaec16
Elías Vasquez <78238476+Evasq710@users.noreply.github.com>
2025-11-11 15:31:14 -0600

    First component

---
+++ 02-first-steps/src/FirstStepsApp.tsx
@@ -0,0 +1,15 @@
+
+export function FirstStepsApp() {
+    return (
+        // <> --> fragmento
+        <> 
+            <h1>Hola mundo :D</h1>
+            <p>Esto es un párrafo</p>
+            <button>Click me</button>
+
+            <div>
+            <h2>Hola dentro de un div</h2>
+            </div>
+        </>
+    )
+}
--- 02-first-steps/src/main.tsx
+++ 02-first-steps/src/main.tsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { FirstStepsApp } from './firstStepsApp'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <FirstStepsApp />
   </StrictMode>,
 )

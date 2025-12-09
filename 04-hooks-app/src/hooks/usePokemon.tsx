8bc8da0d092ce0e45ea908f06169c5d81bb4cd6b
Elías Vasquez <78238476+Evasq710@users.noreply.github.com>
2025-12-09 12:00:02 -0600

    Custom Hook that makes an API call to get Pokemon data - usePokemon

---
+++ 04-hooks-app/src/hooks/usePokemon.tsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react";
+
+
+interface Pokemon {
+  id: number;
+  name: string;
+  imageUrl: string;
+}
+
+interface Props {
+  id: number;
+}
+
+export const usePokemon = ({ id }: Props) => {
+
+  const [pokemon, setPokemon] = useState<Pokemon | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const getPokemonById = async (id: number) => {
+    setIsLoading(true);
+
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+
+    if (!response.ok) {
+      setPokemon(null);
+      setIsLoading(false);
+      return;
+    }
+
+    const data = await response.json();
+
+    setPokemon({
+      id: id,
+      name: data.name,
+      imageUrl: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`,
+    });
+    setIsLoading(false);
+  };
+
+  // Making the call to the API whenever the id changes
+  useEffect(() => {
+    getPokemonById(id);
+  }, [id]);
+
+  return {
+    // Props
+    isLoading,
+    pokemon,
+    // Computed
+    formattedId: id.toString().padStart(3, '0'),
+  }
+}

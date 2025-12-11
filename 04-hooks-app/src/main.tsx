b6e51229b3574eb9011c0392c8768e83b14018ce
Elías Vasquez <78238476+Evasq710@users.noreply.github.com>
2025-12-11 09:00:29 -0600

    TaskApp component

--- 04-hooks-app/src/main.tsx
+++ 04-hooks-app/src/main.tsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { TasksApp } from './05-useReducer/TaskApp'
 // import { TrafficLightWithHook } from './02-useEffect/TrafficLightWithHook'
 // import { PokemonPage } from './03-examples/PokemonPage'
-import { FocusScreen } from './04-useRef/FocusScreen'
+// import { FocusScreen } from './04-useRef/FocusScreen'
 // import { TrafficLightWithEffect } from './02-useEffect/TrafficLightWithEffect'
 // import { TrafficLight } from './01-useState/TrafficLight'
 
@@ -13,6 +14,7 @@ createRoot(document.getElementById('root')!).render(
     {/* <TrafficLightWithEffect /> */}
     {/* <TrafficLightWithHook /> */}
     {/* <PokemonPage /> */}
-    <FocusScreen />
+    {/* <FocusScreen /> */}
+    <TasksApp />
   </StrictMode>,
 )

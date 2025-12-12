cc6af574c6ea254c8c7690c8a835653e2f3202cf
Elías Vasquez <78238476+Evasq710@users.noreply.github.com>
2025-12-12 12:46:48 -0600

    ScrambleWords Component

--- 04-hooks-app/src/main.tsx
+++ 04-hooks-app/src/main.tsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { TasksApp } from './05-useReducer/TaskApp'
+import { ScrambleWords } from './05-useReducer/ScrambleWords'
+// import { TasksApp } from './05-useReducer/TaskApp'
 // import { TrafficLightWithHook } from './02-useEffect/TrafficLightWithHook'
 // import { PokemonPage } from './03-examples/PokemonPage'
 // import { FocusScreen } from './04-useRef/FocusScreen'
@@ -15,6 +16,7 @@ createRoot(document.getElementById('root')!).render(
     {/* <TrafficLightWithHook /> */}
     {/* <PokemonPage /> */}
     {/* <FocusScreen /> */}
-    <TasksApp />
+    {/* <TasksApp /> */}
+    <ScrambleWords />
   </StrictMode>,
 )

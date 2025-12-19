07a1ecbc9ab51037e015dbe8dc85149ce2548a5d
Elías Vasquez <78238476+Evasq710@users.noreply.github.com>
2025-12-19 16:33:35 -0600

    Conditional rendering, depending on session

--- 04-hooks-app/src/09-useContext/pages/about/AboutPage.tsx
+++ 04-hooks-app/src/09-useContext/pages/about/AboutPage.tsx
@@ -1,6 +1,12 @@
+import { UserContext } from "@/09-useContext/context/UserContext"
+import { Button } from "@/components/ui/button";
+import { use } from "react";
 import { Link } from "react-router"
 
 export const AboutPage = () => {
+
+  const { isAuthenticated, logout } = use(UserContext);
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <h1 className="text-4xl font-bold">Página sobre mi</h1>
@@ -8,8 +14,22 @@ export const AboutPage = () => {
 
       <div className="flex flex-col gap-2">
         {/* Anchor tags reload the browser, so it is not recommended to use them */}
-        <Link to="/profile" className="hover:text-blue-500 underline text-2xl">Perfil</Link>
-        <Link to="/login" className="hover:text-blue-500 underline text-2xl">Iniciar sesión</Link>
+
+        {/* Perfil de usuario si tiene sesión */}
+        {
+          isAuthenticated &&
+          <Link to="/profile" className="hover:text-blue-500 underline text-2xl">Perfil</Link>
+        }
+
+        {/* Login/Logout buttons */}
+        {
+          isAuthenticated ?
+            <Button variant="destructive" className="mt-4" onClick={logout}>
+              Salir
+            </Button> :
+            <Link to="/login" className="hover:text-blue-500 underline text-2xl">Iniciar sesión</Link>
+        }
+
       </div>
     </div>
   )
